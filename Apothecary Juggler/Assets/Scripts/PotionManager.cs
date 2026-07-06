58c599d075d9ff6e888252caa8b9aeb4fda38d8e
Devon Nutting <126836214+DevonNutting@users.noreply.github.com>
2026-07-06 11:03:45 -0700

    Potion Manager
    - Created a simple potion manager to spawn in potions at random positions

---
+++ Apothecary Juggler/Assets/Scripts/PotionManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PotionManager : MonoBehaviour
+{
+    [SerializeField] private GameObject parentObject;
+    [SerializeField] private GameObject[] juggleables;
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private float spawnRate = 5f;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnJuggleable), spawnRate, spawnRate);
+    }
+
+    private void SpawnJuggleable()
+    {
+        // Pick a random spawn point
+        int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
+
+        // Pick a random juggleable
+        int randomJuggleIndex = Random.Range(0, juggleables.Length);
+
+        Vector3 spawnPosition = spawnPoints[randomSpawnIndex].position;
+
+        GameObject juggleable = Instantiate(
+            juggleables[randomJuggleIndex],
+            spawnPosition,
+            Quaternion.identity,
+            parentObject.transform
+        );
+
+        Debug.Log($"Spawning {juggleable.name} at {spawnPosition}");
+    }
+}
